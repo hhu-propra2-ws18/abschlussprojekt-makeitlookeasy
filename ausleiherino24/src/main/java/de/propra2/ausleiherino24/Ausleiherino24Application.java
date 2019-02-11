fe5e29177f1d37b94eeb561b44ebc328dee1e06f
Jonathan Peppler <jonathan-peppler@gmx.de>
2019-02-11 12:47:02 +0100

    Added Spring Boot Initalizier

---
+++ ausleiherino24/src/main/java/de/propra2/ausleiherino24/Ausleiherino24Application.java
@@ -0,0 +1,14 @@
+package de.propra2.ausleiherino24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ausleiherino24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ausleiherino24Application.class, args);
+	}
+
+}
+
---
+++ ausleiherino24/src/test/java/de/propra2/ausleiherino24/Ausleiherino24ApplicationTests.java
@@ -0,0 +1,17 @@
+package de.propra2.ausleiherino24;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ausleiherino24ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
