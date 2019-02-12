69142e2753b544304395c1835e263701cba0e51c
WobinRho <28410848+WobinRho@users.noreply.github.com>
2019-02-12 13:26:51 +0100

    updated ProPay data exchange.

--- src/main/java/de/propra2/ausleiherino24/proPayHandler/AccountHandler.java
+++ src/main/java/de/propra2/ausleiherino24/proPayHandler/AccountHandler.java
@@ -10,8 +10,8 @@ import org.springframework.web.client.RestTemplate;
 
 public class AccountHandler {
 	private RestTemplate restTemplate = new RestTemplate();
-	ResponseEntity<Account> accountResponseEntity;
 	private static final String ACCOUNT_URL = "localhost:8888/account";
+
 	public boolean hasValidFunds(String accountName, double requestedFunds){
 		double reserved = 0;
 		Account account = restTemplate.getForObject(ACCOUNT_URL +"/{account}",Account.class, accountName);
@@ -20,13 +20,27 @@ public class AccountHandler {
 		}
 		return account.getAmount() - reserved >= requestedFunds;
 	}
-	public boolean addFunds(String username,double amount){
+
+	public boolean addFunds(String username, double amount){
 		ClientHttpRequestFactory requestFactory = getClientHttpRequestFactory();
 		restTemplate = new RestTemplate(requestFactory);
-		HttpEntity<Account> request = new HttpEntity<>(new Account(username, amount));
-		accountResponseEntity = restTemplate.exchange(ACCOUNT_URL +"/{account}", HttpMethod.POST, request, Account.class);
-		return accountResponseEntity.getStatusCode().equals(HttpStatus.OK);
+		HttpEntity<Double> request = new HttpEntity<>(amount);
+		ResponseEntity<Double> responseEntity = restTemplate.exchange(ACCOUNT_URL +"/{account}", HttpMethod.POST, request, Double.class, username);
+		return responseEntity.getStatusCode().equals(HttpStatus.OK);
 	}
+
+	public boolean transferFunds(String sourceUser, String targetUser, double amount){
+		if(hasValidFunds(sourceUser,amount)) {
+			ClientHttpRequestFactory requestFactory = getClientHttpRequestFactory();
+			restTemplate = new RestTemplate(requestFactory);
+			HttpEntity<Double> request = new HttpEntity<>(amount);
+			ResponseEntity<Double> responseEntity = restTemplate.exchange(ACCOUNT_URL + "/{sourceAccount}/transfer/{targetAccount}", HttpMethod.POST, request, Double.class, sourceUser, targetUser);
+			return responseEntity.getStatusCode().equals(HttpStatus.OK) || responseEntity.getStatusCode().equals(HttpStatus.CREATED);
+		}
+		return false;
+	}
+
+
 
 
 
