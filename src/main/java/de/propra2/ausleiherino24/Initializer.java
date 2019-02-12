70ccb460259c8f85311f6eeb402595acce183ce9
mickpotzkai <mipot101@hhu.de>
2019-02-12 11:40:59 +0100

    Adds Initializer

---
+++ src/main/java/de/propra2/ausleiherino24/Initializer.java
@@ -0,0 +1,30 @@
+package de.propra2.ausleiherino24;
+
+import de.propra2.ausleiherino24.data.UserRepository;
+import de.propra2.ausleiherino24.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.util.Locale;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Component
+public class Initializer implements ServletContextInitializer{
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public void onStartup(final ServletContext servletContext) throws ServletException {
+		userRepository.deleteAll();
+		User user = new User();
+		user.setUsername("user");
+		user.setPassword("password");
+		user.setEmail("user@mail.com");
+		userRepository.save(user);
+	}
+}
