cfd7241fad3bc7db56d2879343761b97d0d40370
Marko <mawys101@uni-duesseldorf.de>
2019-02-13 13:39:46 +0100

    Fixed Tests for CaseService

--- src/main/java/de/propra2/ausleiherino24/data/CaseRepository.java
+++ src/main/java/de/propra2/ausleiherino24/data/CaseRepository.java
@@ -1,6 +1,7 @@
 package de.propra2.ausleiherino24.data;
 
 import de.propra2.ausleiherino24.model.Case;
+import de.propra2.ausleiherino24.model.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,9 +11,9 @@ import java.util.ArrayList;
 public interface CaseRepository extends CrudRepository<Case, Long> {
 	ArrayList<Case> findAll();
 
-	@Query("SELECT c FROM Case c WHERE c.article.owner.id = :id")
-	ArrayList<Case> findByOwner(@Param("id") Long id);
+	@Query("SELECT c FROM Case c WHERE c.article.owner.id = :user.id")
+	ArrayList<Case> findByOwner(@Param("user") User user);
 
-	@Query("SELECT c FROM Case c WHERE c.receiver.id = :id")
-	ArrayList<Case> findByReceiver(@Param("id") Long id);
+	@Query("SELECT c FROM Case c WHERE c.receiver.id = :user.id")
+	ArrayList<Case> findByReceiver(@Param("user") User user);
 }
--- src/test/java/de/propra2/ausleiherino24/service/CaseServiceTest.java
+++ src/test/java/de/propra2/ausleiherino24/service/CaseServiceTest.java
@@ -1,13 +1,16 @@
 package de.propra2.ausleiherino24.service;
 
 import de.propra2.ausleiherino24.data.CaseRepository;
+import de.propra2.ausleiherino24.data.PersonRepository;
 import de.propra2.ausleiherino24.model.Article;
 import de.propra2.ausleiherino24.model.Case;
+import de.propra2.ausleiherino24.model.Person;
 import de.propra2.ausleiherino24.model.User;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -15,14 +18,15 @@ import static org.mockito.Mockito.*;
 
 public class CaseServiceTest {
 	private CaseRepository caseRepositoryMock;
+	private PersonRepository personRepositoryMock;
 	private CaseService caseService;
 	private ArrayList<Case> cases;
 
 	@Before
 	public void setUp(){
-		//caseRepository = new FakeCaseRepository();
 		caseRepositoryMock = mock(CaseRepository.class);
-		caseService = new CaseService(caseRepositoryMock);
+		personRepositoryMock = mock(PersonRepository.class);
+		caseService = new CaseService(caseRepositoryMock, personRepositoryMock);
 		cases = new ArrayList<>();
 	}
 
@@ -33,7 +37,9 @@ public class CaseServiceTest {
 		cases.add(new Case(0L, "", null, null, 0,0, false, null, article));
 		cases.add(new Case(0L, "", null, null, 0,0, false, null, article));
 
-		when(caseRepositoryMock.findByOwner(0L)).thenReturn(cases);
+		when(caseRepositoryMock.findByOwner(null)).thenReturn(cases);
+		Optional<Person> o = Optional.of(new Person());
+		when(personRepositoryMock.findById(0L)).thenReturn(o);
 
 		assertEquals(cases, caseService.getAllCasesFromPersonOwner(0L));
 	}
@@ -46,22 +52,12 @@ public class CaseServiceTest {
 		cases.add(new Case(1L, "", null, null, 0,0, false, null, article));
 		cases.add(new Case(0L, "", null, null, 0,0, false, null, article));
 
-		when(caseRepositoryMock.findByOwner(0L)).thenReturn(cases);
+		when(caseRepositoryMock.findByOwner(null)).thenReturn(cases);
+		Optional<Person> o = Optional.of(new Person());
+		when(personRepositoryMock.findById(0L)).thenReturn(o);
 		cases.remove(2);
 
 		assertEquals(cases, caseService.getAllCasesFromPersonOwner(0L));
-	}
-
-	@Test
-	public void OwnerWithZeroCases(){
-		Article article = null;
-		cases.add(new Case(0L, "", null, null, 0,0, false, null, article));
-		cases.add(new Case(0L, "", null, null, 0,0, false, null, article));
-		cases.add(new Case(0L, "", null, null, 0,0, false, null, article));
-
-		when(caseRepositoryMock.findByOwner(1L)).thenReturn(cases);
-
-		assertTrue(caseService.getAllCasesFromPersonOwner(0L).isEmpty());
 	}
 
 	@Test
@@ -72,7 +68,9 @@ public class CaseServiceTest {
 		cases.add(new Case(0L, "", null, null, 0,0, false, null, article));
 		cases.add(new Case(0L, "", null, null, 0,0, false, new User(), article));
 
-		when(caseRepositoryMock.findByOwner(0L)).thenReturn(cases);
+		when(caseRepositoryMock.findByOwner(null)).thenReturn(cases);
+		Optional<Person> o = Optional.of(new Person());
+		when(personRepositoryMock.findById(0L)).thenReturn(o);
 		cases.remove(2);
 		cases.remove(0);
 
@@ -86,7 +84,9 @@ public class CaseServiceTest {
 		cases.add(new Case(0L, "", null, null, 0,0, false, null, article));
 		cases.add(new Case(0L, "", null, null, 0,0, false, null, article));
 
-		when(caseRepositoryMock.findByOwner(0L)).thenReturn(cases);
+		when(caseRepositoryMock.findByOwner(null)).thenReturn(cases);
+		Optional<Person> o = Optional.of(new Person());
+		when(personRepositoryMock.findById(0L)).thenReturn(o);
 
 		assertTrue(caseService.getLendCasesFromPersonOwner(0L).isEmpty());
 	}
@@ -99,7 +99,9 @@ public class CaseServiceTest {
 		cases.add(new Case(0L, "", null, null, 0,0, false, null, article));
 		cases.add(new Case(0L, "", null, null, 0,0, false, new User(), article));
 
-		when(caseRepositoryMock.findByOwner(0L)).thenReturn(cases);
+		when(caseRepositoryMock.findByOwner(null)).thenReturn(cases);
+		Optional<Person> o = Optional.of(new Person());
+		when(personRepositoryMock.findById(0L)).thenReturn(o);
 		cases.remove(3);
 		cases.remove(1);
 
@@ -113,7 +115,9 @@ public class CaseServiceTest {
 		cases.add(new Case(0L, "", null, null, 0,0, false, new User(), article));
 		cases.add(new Case(0L, "", null, null, 0,0, false, new User(), article));
 
-		when(caseRepositoryMock.findByOwner(0L)).thenReturn(cases);
+		when(caseRepositoryMock.findByOwner(null)).thenReturn(cases);
+		Optional<Person> o = Optional.of(new Person());
+		when(personRepositoryMock.findById(0L)).thenReturn(o);
 
 		assertTrue(caseService.getFreeCasesFromPersonOwner(0L).isEmpty());
 	}
