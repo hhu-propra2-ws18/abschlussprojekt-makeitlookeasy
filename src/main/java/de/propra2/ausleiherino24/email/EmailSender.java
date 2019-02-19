1dd6d6ecb0db16c65c06da95a97090277c5c6654
amincs <boulahbel@mail.de>
2019-02-19 14:28:40 +0100

    Updated EmailSender and corresponding tests

--- src/main/java/de/propra2/ausleiherino24/email/EmailSender.java
+++ src/main/java/de/propra2/ausleiherino24/email/EmailSender.java
@@ -1,10 +1,13 @@
 package de.propra2.ausleiherino24.email;
 
-import de.propra2.ausleiherino24.model.Conflict;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Component;
+
+import de.propra2.ausleiherino24.model.Conflict;
+import de.propra2.ausleiherino24.model.User;
+import de.propra2.ausleiherino24.service.UserService;
 
 @Component
 public class EmailSender {
@@ -12,22 +15,26 @@ public class EmailSender {
 	private EmailConfig config;
 	private JavaMailSenderImpl mailSender;
 	private SimpleMailMessage message;
+	private UserService userService;
 
 	@Autowired
 	public EmailSender(EmailConfig config, JavaMailSenderImpl mailSender,
-			SimpleMailMessage message) {
+			SimpleMailMessage message, UserService userService) {
 		this.config = config;
 		this.mailSender = mailSender;
 		this.message = message;
+		this.userService = userService;
 	}
 
-	public void sendEmail(Conflict conflict) {
+	public void sendEmail(Conflict conflict) throws Exception {
 		mailSender.setHost(config.getHost());
 		mailSender.setPort(config.getPort());
 		mailSender.setUsername(config.getUsername());
 		mailSender.setPassword(config.getPassword());
 
-		message.setFrom(conflict.getConflictReporter().getEmail());
+		User user = userService.findUserByUsername(conflict.getConflictReporterUsername());
+
+		message.setFrom(user.getEmail());
 		message.setTo("Clearing@Service.com"); // FakeEmail -> does not matter what goes in here
 		message.setSubject("Conflicting Case id: " + conflict.getConflictedCase().getId());
 		message.setText(conflict.getConflictDescription());
