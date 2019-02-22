db20de0b9b4301da3ccf040a01399f93032ef1f6
amincs <boulahbel@mail.de>
2019-02-22 13:24:22 +0100

    Updated ConflictService+ConflictController and Adjusted ConflictServiceTest @PairProgramming:Giovanni

--- src/main/java/de/propra2/ausleiherino24/web/ConflictController.java
+++ src/main/java/de/propra2/ausleiherino24/web/ConflictController.java
@@ -52,15 +52,7 @@ public class ConflictController {
         if(!optionalCase.isPresent()) {
             return "redirect:/myOverview?returned&conflictfailed";
         }
-
-        Conflict conflict = new Conflict();
-        conflict.setConflictDescription(conflictDescription);
-        conflict.setConflictedCase(optionalCase.get());
-        conflict.setConflictReporterUsername(optionalCase.get().getArticle().getOwner().getUsername());
-        caseService.conflictOpened(id);
-
-        conflictService.saveConflict(conflict, optionalCase.get().getOwner());
-        //conflictService.sendConflictEmail(conflict);
+        conflictService.openConflict(optionalCase.get(), conflictDescription);
 
         return "redirect:/myOverview?returned&openedconflict";
     }
