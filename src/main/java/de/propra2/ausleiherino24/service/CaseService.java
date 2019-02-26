3eae90f8921a78db021f98550c8ca348d95ce578
WobinRho <28410848+WobinRho@users.noreply.github.com>
2019-02-26 11:12:17 +0100

    added date to PPTransaction
    rvwd by marius herrmann

--- src/main/java/de/propra2/ausleiherino24/service/CaseService.java
+++ src/main/java/de/propra2/ausleiherino24/service/CaseService.java
@@ -115,6 +115,7 @@ public class CaseService {
             final PPTransaction ppTransaction = new PPTransaction();
             ppTransaction.setLendingCost(totalCost);
             ppTransaction.setCautionPaid(false);
+            ppTransaction.setDate(new Date().getTime());
 
             final Case aCase = new Case();
             aCase.setArticle(articleService.findArticleById(articleId));
@@ -163,9 +164,9 @@ public class CaseService {
 
         //Check whether the article is not reserved in this period of time
         final boolean articleRented = articleNotRented(id);
+
         if (articleRented && accountHandler.hasValidFunds(c)) {
             c.setRequestStatus(Case.REQUEST_ACCEPTED);
-            c.getPpTransaction().setDate(new Date().getTime());
             reservationHandler.handleReservedMoney(c);
             caseRepository.save(c);
             return 1;
