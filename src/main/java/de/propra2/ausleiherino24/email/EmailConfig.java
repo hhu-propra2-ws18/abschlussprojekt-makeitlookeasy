32e437d70df266a5f70c673a4544500139e66a39
chwba <22568014+chwba@users.noreply.github.com>
2019-02-28 02:02:01 +0100

    * set source-sets to main for all code-analysis tools * added dependency for ConfigurationProperties annotation processor * cleanup

--- src/main/java/de/propra2/ausleiherino24/email/EmailConfig.java
+++ src/main/java/de/propra2/ausleiherino24/email/EmailConfig.java
@@ -3,11 +3,13 @@ package de.propra2.ausleiherino24.email;
 import java.util.Map;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
 @ConfigurationProperties(prefix = "spring.mail")
+@EnableConfigurationProperties
 public class EmailConfig {
 
     private String host;
