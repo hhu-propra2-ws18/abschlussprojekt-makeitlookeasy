a56ca553e4b09bec4f8164d9393c9d1bc96388de
chwba <22568014+chwba@users.noreply.github.com>
2019-03-01 13:38:42 +0100

    * added Spring-Security part to documentation * rearranged one annotation for better readability in the main-test class * switched to BCryptPasswordEncoder

--- src/main/java/de/propra2/ausleiherino24/security/SecurityConfiguration.java
+++ src/main/java/de/propra2/ausleiherino24/security/SecurityConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
@@ -65,8 +65,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
      * @return passwordEncoder Entity to be used
      */
     @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return new PasswordEncoder() {
+    public BCryptPasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder() {
             @Override
             public String encode(final CharSequence rawPassword) {
                 return rawPassword.toString();
